032dd2e3d740f8023d809ef78fd9bea5a6da143c
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-07-23 15:35:23 +0300

    create the server #6

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
+const http = require('http');
+
 const router = require('./router');
+
+const port = process.env.PORT || 3030;
+
+const server = http.createServer(router);
+
+server.listen(port, () => {
+  console.log(`now server listining to http://localhost:${port} now`);
+});
