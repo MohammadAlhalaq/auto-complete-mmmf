7ac23c2d128597faa063631bc97569aee0807dee
mossa-Sammer <mossa708496@gmail.com>
2019-07-24 16:39:42 +0300

    fix-bugs relates #25

--- test/test.js
+++ test/test.js
@@ -10,12 +10,3 @@ test('test filterData function', (t) => {
   t.deepEqual(actual, excpected, `expected Xllison Horne but got ${actual}`);
   t.end();
 });
-
-test('find object', (t) => {
-  const excpected = {};
-
-  const actual = {};
-
-  t.deepEqual(actual, excpected, 'test Failed');
-  t.end();
-});
