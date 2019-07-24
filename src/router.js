38a8b27ee601859e7e13b69bacefcc4102ef7538
fadi omar <fadeomar2015@gmail.com>
2019-07-24 09:46:30 +0300

    solve unused lines

--- src/router.js
+++ src/router.js
@@ -1,29 +1,21 @@
-const path = require('path');
 
-const fs = require('fs');
-
-const handelhome = require('./handlers/handelHome')
+const handelhome = require('./handlers/handelHome');
 
 const router = (req, res) => {
-    const endpoint = req.url;
-    const arrayPath = endpoint.split('/');
-    const ext = endpoint.split('.')[1];
-    const extType = {
-        html: 'text/html',
-        css: 'text/css',
-        jpg: 'image/jpg',
-        ico: 'image/x-icon',
-        png:'image/png',
-        js: 'application/javascript',
-    }
-    console.log(endpoint)
-    if(endpoint === '/'){
-        handelhome(res)
-    }else {
-
-    }
-    
-
-}
+  const endpoint = req.url;
+  // const arrayPath = endpoint.split('/');
+  // const ext = endpoint.split('.')[1];
+  // const extType = {
+  // html: 'text/html',
+  //   css: 'text/css',
+  //   jpg: 'image/jpg',
+  //   ico: 'image/x-icon',
+  //   png:'image/png',
+  //   js: 'application/javascript',
+  // }
+  if (endpoint === '/') {
+    handelhome(res);
+  }
+};
 
 module.exports = router;
